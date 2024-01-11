581222fd5211eb7e0762c71897ad2d6119fc100f
Alex Axthelm <aaxthelm@RMI.org>
2024-01-11 20:29:09 +0100

    Add simple reorder test

--- tests/testthat/test-reexport.R
+++ tests/testthat/test-reexport.R
@@ -8,6 +8,10 @@ test_dir <- tempdir()
 withr::defer(unlink(test_dir))
 
 empty_groups <- data.frame()
+simple_groups <- tibble::tribble(
+  ~investor_name, ~portfolio_name,
+  "Simple Investor", "Simple Portfolio"
+)
 
 test_that("re-exporting simple file works.", {
   test_file <- system.file(
@@ -154,14 +158,34 @@ test_that("re-exporting simple file with all columns works", {
     input_filepath = test_file,
     output_directory = test_dir
   )
-  groups <- tibble::tribble(
-    ~investor_name, ~portfolio_name,
-    "Simple Investor", "Simple Portfolio"
+  expect_simple_reexport(
+    output_dir = test_dir,
+    metadata = metadata,
+    groups = simple_groups,
+    input_digest = filehash,
+    input_filename = basename(test_file),
+    input_entries = 1
+  )
+})
+
+test_that("re-exporting robust against column order - simple.", {
+  test_file <- system.file(
+    "extdata", "portfolios", "simple_reordered.csv",
+    package = "workflow.portfolio.parsing"
+  )
+  filehash <- digest::digest(
+    object = test_file,
+    file = TRUE,
+    algo = "md5"
+  )
+  metadata <- reexport_portfolio(
+    input_filepath = test_file,
+    output_directory = test_dir
   )
   expect_simple_reexport(
     output_dir = test_dir,
     metadata = metadata,
-    groups = groups,
+    groups = empty_groups,
     input_digest = filehash,
     input_filename = basename(test_file),
     input_entries = 1
