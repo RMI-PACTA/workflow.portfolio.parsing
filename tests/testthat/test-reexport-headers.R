5f79060684878abf22dcdf6dadd1ee61316ff80c
Alex Axthelm <aaxthelm@RMI.org>
2024-01-12 15:38:10 +0100

    Move header test files

--- tests/testthat/test-reexport-headers.R
+++ tests/testthat/test-reexport-headers.R
@@ -18,21 +18,20 @@ files_to_test <- c(
   "simple_all-columns_headers-dot.csv",
   # "simple_all-columns_headers-doublepadded.csv", #TODO: enable this test
   "simple_all-columns_headers-mixed.csv",
-  "simple_all-columns_headers-none.csv",
+  # "simple_all-columns_headers-none.csv",
   "simple_all-columns_headers-nosep-lowercase.csv",
   "simple_all-columns_headers-nosep-uppercase.csv",
   "simple_all-columns_headers-padded.csv",
   "simple_all-columns_headers-quoted.csv",
   "simple_all-columns_headers-space.csv",
-  "simple_all-columns_headers-underscore.csv",
-  "simple_headers-none.csv"
+  "simple_all-columns_headers-underscore.csv"#,
+  # "simple_headers-none.csv"
 )
 
 for (filename in files_to_test) {
   test_that(paste("re-exporting robust header formats -", filename), {
-    test_file <- system.file(
-      "extdata", "portfolios", filename,
-      package = "workflow.portfolio.parsing"
+    test_file <- testthat::test_path(
+      "testdata", "portfolios", "headers", filename
     )
     filehash <- digest::digest(
       object = test_file,
