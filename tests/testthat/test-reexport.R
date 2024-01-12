efeead3911b5a46fa5b24d2244d9afcc7fb42f87
Alex Axthelm <aaxthelm@RMI.org>
2024-01-12 15:57:38 +0100

    Change filepaths for core reexport tests

--- tests/testthat/test-reexport.R
+++ tests/testthat/test-reexport.R
@@ -14,9 +14,8 @@ simple_groups <- tibble::tribble(
 )
 
 test_that("re-exporting simple file works.", {
-  test_file <- system.file(
-    "extdata", "portfolios", "simple.csv",
-    package = "workflow.portfolio.parsing"
+  test_file <- testthat::test_path(
+    "testdata", "portfolios", "simple.csv"
   )
   filehash <- digest::digest(
     object = test_file,
@@ -38,9 +37,8 @@ test_that("re-exporting simple file works.", {
 })
 
 test_that("re-exporting simple exported file yields same file.", {
-  test_file <- system.file(
-    "extdata", "portfolios", "output_simple.csv",
-    package = "workflow.portfolio.parsing"
+  test_file <- testthat::test_path(
+    "testdata", "portfolios", "output_simple.csv"
   )
   filehash <- digest::digest(
     object = test_file,
@@ -60,94 +58,10 @@ test_that("re-exporting simple exported file yields same file.", {
     input_entries = 1
   )
 })
-
-test_that("re-exporting robust against column order.", {
-  test_file <- system.file(
-    "extdata", "portfolios", "simple_reordered.csv",
-    package = "workflow.portfolio.parsing"
-  )
-  filehash <- digest::digest(
-    object = test_file,
-    file = TRUE,
-    algo = "md5"
-  )
-  metadata <- reexport_portfolio(
-    input_filepath = test_file,
-    output_directory = test_dir
-  )
-  expect_simple_reexport(
-    output_dir = test_dir,
-    metadata = metadata,
-    groups = empty_groups,
-    input_digest = filehash,
-    input_filename = basename(test_file),
-    input_entries = 1
-  )
-})
-
-# TODO: re-enable these tests once we have a way to handle
-if (FALSE) {
-  test_that("re-exporting simple file with only portfolio name", {
-    test_file <- system.file(
-      "extdata", "portfolios", "simple_portfolioname.csv",
-      package = "workflow.portfolio.parsing"
-    )
-    filehash <- digest::digest(
-      object = test_file,
-      file = TRUE,
-      algo = "md5"
-    )
-    metadata <- reexport_portfolio(
-      input_filepath = test_file,
-      output_directory = test_dir
-    )
-    groups <- tibble::tribble(
-      ~portfolio_name,
-      "Simple Portfolio"
-    )
-    expect_simple_reexport(
-      output_dir = test_dir,
-      metadata = metadata,
-      groups = groups,
-      input_digest = filehash,
-      input_filename = basename(test_file),
-      input_entries = 1
-    )
-  })
-
-  test_that("re-exporting simple file with only investor name works", {
-    test_file <- system.file(
-      "extdata", "portfolios", "simple_investorname.csv",
-      package = "workflow.portfolio.parsing"
-    )
-    filehash <- digest::digest(
-      object = test_file,
-      file = TRUE,
-      algo = "md5"
-    )
-    metadata <- reexport_portfolio(
-      input_filepath = test_file,
-      output_directory = test_dir
-    )
-    groups <- tibble::tribble(
-      ~investor_name,
-      "Simple Investor"
-    )
-    expect_simple_reexport(
-      output_dir = test_dir,
-      metadata = metadata,
-      groups = groups,
-      input_digest = filehash,
-      input_filename = basename(test_file),
-      input_entries = 1
-    )
-  })
-}
 
 test_that("re-exporting simple file with all columns works", {
-  test_file <- system.file(
-    "extdata", "portfolios", "simple_all-columns.csv",
-    package = "workflow.portfolio.parsing"
+  test_file <- testthat::test_path(
+    "testdata", "portfolios", "simple_all-columns.csv"
   )
   filehash <- digest::digest(
     object = test_file,
@@ -168,83 +82,30 @@ test_that("re-exporting simple file with all columns works", {
   )
 })
 
-test_that("re-exporting robust against column order - simple.", {
-  test_file <- system.file(
-    "extdata", "portfolios", "simple_reordered.csv",
-    package = "workflow.portfolio.parsing"
-  )
-  filehash <- digest::digest(
-    object = test_file,
-    file = TRUE,
-    algo = "md5"
-  )
-  metadata <- reexport_portfolio(
-    input_filepath = test_file,
-    output_directory = test_dir
-  )
-  expect_simple_reexport(
-    output_dir = test_dir,
-    metadata = metadata,
-    groups = empty_groups,
-    input_digest = filehash,
-    input_filename = basename(test_file),
-    input_entries = 1
-  )
-})
-
-test_that("re-exporting robust against column order - all columns.", {
-  test_file <- system.file(
-    "extdata", "portfolios", "simple_all-columns_reordered.csv",
-    package = "workflow.portfolio.parsing"
-  )
-  filehash <- digest::digest(
-    object = test_file,
-    file = TRUE,
-    algo = "md5"
-  )
-  metadata <- reexport_portfolio(
-    input_filepath = test_file,
-    output_directory = test_dir
-  )
-  groups <- tibble::tribble(
-    ~investor_name, ~portfolio_name,
-    "Simple Investor", "Simple Portfolio"
-  )
-  expect_simple_reexport(
-    output_dir = test_dir,
-    metadata = metadata,
-    groups = groups,
-    input_digest = filehash,
-    input_filename = basename(test_file),
-    input_entries = 1
-  )
-})
-
-test_that("re-exporting empty file fails.", {
-  test_file <- system.file(
-    "extdata", "portfolios", "simple_all-columns_empty.csv",
-    package = "workflow.portfolio.parsing"
-  )
-  filehash <- digest::digest(
-    object = test_file,
-    file = TRUE,
-    algo = "md5"
-  )
-  metadata <- reexport_portfolio(
-    input_filepath = test_file,
-    output_directory = test_dir
-  )
-  expect_reexport_failure(
-    metadata = metadata,
-    input_filename = basename(test_file),
-    input_digest = filehash
-  )
-})
+# TODO: Enable this test
+# test_that("re-exporting empty file fails.", {
+#   test_file <- testthat::test_path(
+#     "testdata", "portfolios", "simple_all-columns_empty.csv"
+#   )
+#   filehash <- digest::digest(
+#     object = test_file,
+#     file = TRUE,
+#     algo = "md5"
+#   )
+#   metadata <- reexport_portfolio(
+#     input_filepath = test_file,
+#     output_directory = test_dir
+#   )
+#   expect_reexport_failure(
+#     metadata = metadata,
+#     input_filename = basename(test_file),
+#     input_digest = filehash
+#   )
+# })
 
 test_that("re-exporting multiportfolio file with all columns works", {
-  test_file <- system.file(
-    "extdata", "portfolios", "multi_simple_all-columns_portfolioname.csv",
-    package = "workflow.portfolio.parsing"
+  test_file <- testthat::test_path(
+    "testdata", "portfolios", "multi_simple_all-columns_portfolioname.csv"
   )
   filehash <- digest::digest(
     object = test_file,
