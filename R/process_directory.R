0c623d1cee22ed0775767eb5c34e00bb579e182b
Alex Axthelm <aaxthelm@RMI.org>
2024-01-30 12:25:49 +0100

    feat(app): Process directory
    Update process directory and add initial tests

--- R/process_directory.R
+++ R/process_directory.R
@@ -6,11 +6,14 @@ process_directory <- function(
   # Get the list of files in the directory
   files <- list.files(input_directory, full.names = TRUE)
   # Process each file
+  all_summaries <- list()
   for (file in files) {
-    reexport_portfolio(
+    portfolio_summary <- reexport_portfolio(
       input_filepath = file,
       output_directory = output_directory
     )
+    all_summaries <- c(all_summaries, list(portfolio_summary))
   }
   logger::log_info("Done processing directory.")
+  return(all_summaries)
 }
