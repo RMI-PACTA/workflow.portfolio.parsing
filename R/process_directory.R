4187df0f82ba2c44444bfb9855cd251216f6fa56
Alex Axthelm <aaxthelm@RMI.org>
2024-01-31 10:30:06 +0100

    feat(app): #13 Add metadata validation to directory processing
    Closes: #13

--- R/process_directory.R
+++ R/process_directory.R
@@ -1,7 +1,12 @@
 #' @export
 process_directory <- function(
   input_directory = "/mnt/input",
-  output_directory = "/mnt/output"
+  output_directory = "/mnt/output",
+  validate = TRUE,
+  schema_file = system.file(
+    "extdata", "schema", "metadata.json",
+    package = "workflow.portfolio.parsing"
+  )
 ) {
   # Get the list of files in the directory
   files <- list.files(input_directory, full.names = TRUE)
@@ -15,5 +20,32 @@ process_directory <- function(
     all_summaries <- c(all_summaries, list(portfolio_summary))
   }
   logger::log_info("Done processing directory.")
+
+  logger::log_info("Preparing metadata JSON file.")
+  if (validate) {
+    logger::log_info("Validating output.")
+    sch <- jsonvalidate::json_schema[["new"]](
+      schema = schema_file,
+      strict = TRUE,
+      engine = "ajv"
+    )
+    summaries_json <- jsonlite::prettify(
+      sch[["serialise"]](all_summaries)
+    )
+    json_is_valid <- sch[["validate"]](summaries_json, verbose = TRUE)
+    if (json_is_valid) {
+      logger::log_debug("JSON is valid.")
+    } else {
+      logger::log_error("JSON is not valid.")
+    }
+  } else {
+    logger::log_warn("Skipping JSON validation.")
+    jsonlite::toJSON(
+      x = all_summaries,
+      pretty = TRUE,
+      auto_unbox = TRUE
+    )
+  }
+
   return(all_summaries)
 }
