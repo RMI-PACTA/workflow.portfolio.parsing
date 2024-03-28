a0df5d5f0fd2098dade262ef9dee7f9ade4b4f8b
Alex Axthelm <aaxthelm@RMI.org>
2024-03-28 12:59:54 +0100

    re-add tests with simple portfolios

---
+++ tests/testthat/helper-portfolios.R
@@ -0,0 +1,26 @@
+# Groups
+empty_groups <- data.frame()
+
+simple_groups <- tibble::tribble(
+  ~investor_name, ~portfolio_name,
+  "Simple Investor", "Simple Portfolio"
+)
+
+# Portfolios
+simple_portfolio <- tibble::tribble(
+  ~isin, ~market_value, ~currency,
+  "GB0007980591", 10000L, "USD"
+)
+
+simple_portfolio_all_columns <- dplyr::select(
+  .data = dplyr::mutate(
+    .data = simple_portfolio,
+    portfolio_name = "Simple Portfolio",
+    investor_name = "Simple Investor"
+    ),
+  investor_name,
+  portfolio_name,
+  isin,
+  market_value,
+  currency
+)
